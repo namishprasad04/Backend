de7c4b822d4c4268582fb8710018e53fd600bdf7
Namish Prasad <namishprasad180@gmail.com>
2024-08-04 17:05:19 +0530

    added user and video model

--- src/app.js
+++ src/app.js
@@ -8,8 +8,8 @@ app.use(cors({origin: process.env.CORS_ORIGIN,credentials: true,}));
 
 app.use(express.json({limit: "16kb",}));
 app.use(express.urlencoded({extended: true,limit: "16kb",}));
-app.use(express.static("public"));
-app.use(cookieParser());
+app.use(express.static("public")); //Public assets - to store images,files,pdf on our server
+app.use(cookieParser()); //to perform CRUD operations on users/clients browser using cookies
 
 
 export default app;
